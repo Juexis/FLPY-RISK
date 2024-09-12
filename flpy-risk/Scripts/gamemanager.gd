e83f6e8cd583942144360dbe90bb93002473479d
Juexis <77648644+Juexis@users.noreply.github.com>
2024-09-12 01:09:04 -0400

    janky pillar spawn

--- flpy-risk/Scripts/gamemanager.gd
+++ flpy-risk/Scripts/gamemanager.gd
@@ -1,5 +1,5 @@
 extends Node2D
-var score = 0
+@export var score = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,3 +10,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	print(score)
+
+func spawn_pillars():
+	var mass = preload("res://Scenes/mass_pillar.tscn").instantiate()
+	$"../x/PathFollow2D".progress_ratio = randf()
+	mass.global_position.x = $"../x/PathFollow2D".global_position.x
+	add_child(mass)
+
+func _on_timer_timeout() -> void:
+	spawn_pillars()
--- flpy-risk/Scripts/mass_pillar.gd
+++ flpy-risk/Scripts/mass_pillar.gd
@@ -2,15 +2,18 @@ extends RigidBody2D
 @onready var pointline: Area2D = $Area2D
 const speed = -3
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position += Vector2(speed, 0)
+	
+	# removes pillars after offscreen
+	if position.x < -100:
+		queue_free()
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	GameManager.score += 1
